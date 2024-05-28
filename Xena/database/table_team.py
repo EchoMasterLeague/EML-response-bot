3095700e6e17b7e82077860dca936937c9969829
EchoMasterLeague <155692479+EchoMasterLeague@users.noreply.github.com>
2024-05-28 16:25:41 -0400

    Update table_team.py

--- Xena/database/table_team.py
+++ Xena/database/table_team.py
@@ -23,7 +23,7 @@ class TeamTable(BaseTable):
         """Initialize the Team Table class"""
         super().__init__(db, constants.LEAGUE_DB_TAB_TEAM, TeamRecord, TeamFields)
 
-    async def create_team_record(self, team_name: str) -> TeamRecord:
+    async def create_team_record(self, team_name: str, player: str) -> TeamRecord:
         """Create a new Team record"""
         # Check for existing records to avoid duplication
         existing_record = await self.get_team_record(team_name=team_name)
@@ -33,6 +33,9 @@ class TeamTable(BaseTable):
         record_list = [None] * len(TeamFields)
         record_list[TeamFields.team_name] = team_name
         record_list[TeamFields.status] = TeamStatus.INACTIVE
+        record_list[TeamFields.team_region] = await player.get_field(
+            PlayerFields.region
+        )
         new_record = await self.create_record(record_list, TeamFields)
         # Insert the new record into the database
         await self.insert_record(new_record)
