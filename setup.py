f8f815d0388520c3cbb34008ce2fd2563d100d1e
tekemperor <1448055+tekemperor@users.noreply.github.com>
2024-06-16 09:45:29 -0400

    update owner

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Discord bot for Echo Master League Management',
+    name="eml-response-bot",
+    version="0.2.0",
+    description="Discord bot for Echo Master League Management",
     long_description=readme,
-    author='Orthura',
-    author_email='orthura@gmail.com',
-    url='https://github.com/EchoMasterLeague/EML-response-bot',
+    author="Echo Master League",
+    author_email="echomasterleague@gmail.com",
+    url="https://github.com/EchoMasterLeague/EML-response-bot",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=("tests", "docs")),
 )
-
