1648d70dc154de8fbd35174c75add9ec4aacbac8
tekemperor <1448055+tekemperor@users.noreply.github.com>
2024-06-17 08:42:18 -0400

    improve output of uuid generator

--- src/eml-bot-arena/bot_commands/admin_generate_uuid.py
+++ src/eml-bot-arena/bot_commands/admin_generate_uuid.py
@@ -15,13 +15,20 @@ async def admin_generate_uuid(database: FullDatabase, interaction: discord.Inter
         #######################################################################
         #                             PROCESSING                              #
         #######################################################################
-        uuid = general_helpers.random_id()
 
         #######################################################################
         #                              RESPONSE                               #
         #######################################################################
+        now_epoch = await general_helpers.epoch_timestamp()
+        monday_epoch = await general_helpers.upcoming_monday()
         response_dictionary = {
-            "uuid": uuid,
+            "uuid": await general_helpers.random_id(),
+            "now_epoch": now_epoch,
+            "now_utc": await general_helpers.iso_timestamp(now_epoch),
+            "now_eml": f"{await general_helpers.eml_date(now_epoch)} {await general_helpers.eml_time(now_epoch)}",
+            "monday_epoch": monday_epoch,
+            "monday_utc": await general_helpers.iso_timestamp(monday_epoch),
+            "monday_eml": f"{await general_helpers.eml_date(monday_epoch)} {await general_helpers.eml_time(monday_epoch)}",
         }
         response_code_block = await discord_helpers.code_block(
             await general_helpers.format_json(response_dictionary), "json"
